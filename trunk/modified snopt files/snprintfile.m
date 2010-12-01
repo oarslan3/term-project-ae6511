d17634b38fc44299632f621e8f116a5ad750b392
bigelowms <bigelowms@d2aa24e9-9d6e-5d1f-b88e-75ee46c503b6>
2010-12-01 09:58:01 +0000

    Added modified snopt files - they need to go in your snopt-dis/matlab folder!  The mexw64 may or may not work right - it should be compared with the one posted on T-square.  I think it's smaller because it's a release version so it'll run faster, but I may have missed some things - but I get no errors...  Regardless, the m files need to go in the snopt-dis/matlab folder!

---
+++ trunk/modified snopt files/snprintfile.m
@@ -0,0 +1,26 @@
+% function snprintfile( filename )
+%     Causes SNOPT to write detailed information about its progress in
+%     solving the current problem to the file named in "filename."
+%
+%     "snprintfile off" causes SNOPT to stop writing to filename,
+%     and close the file.
+%
+%     Note that until the file has been closed, it is unlikely to contain
+%     all of the output.
+%     WARNING:  Do not use snset() or snseti() to set the print file.
+
+function snprintfile( filename )
+
+%openprintfile  = snoptcmex( 0, 'SetPrintFile'   );
+%closeprintfile = snoptcmex( 0, 'ClosePrintFile' );
+
+openprintfile  = 10;
+closeprintfile = 12;
+
+if strcmp( filename, 'off' )
+  snoptcmex( closeprintfile );
+elseif strcmp( filename, 'on' )
+  snoptcmex( openprintfile, 'print.out' );
+else
+  snoptcmex( openprintfile, filename );
+end
